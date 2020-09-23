42fe499005e33f6e4ee1b5ac2ac0be5d51cc903a
Bernardo Meurer <bernardo@meurer.org>
2020-09-23 03:14:13 -0700

    default: remove unused deps

--- default.nix
+++ default.nix
@@ -2,12 +2,10 @@ let
   pkgs = import ./nix;
 in
 pkgs.callPackage
-  ({ naersk, cargo, gcc, pkgconfig, raspberrypi-tools, llvmPackages }:
+  ({ naersk, cargo }:
     naersk.buildPackage {
       name = "hyperpixel_init";
       src = ./.;
-      nativeBuildInputs = [ cargo gcc pkgconfig ];
-      buildInputs = [ raspberrypi-tools ];
-      LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
+      nativeBuildInputs = [ cargo ];
     })
 { }
