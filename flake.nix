e52e6e1d113b028f0c85e3f3177efeab36c36841
Bernardo Meurer <bernardo@meurer.org>
2021-01-14 20:16:43 -0800

    changed: add proper devShell to flake.nix

--- flake.nix
+++ flake.nix
@@ -13,25 +13,37 @@
   };
 
   outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      defaultPackage =
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          target = "aarch64-unknown-linux-gnu";
-          toolchain = with fenix.packages.${system}; combine [
-            minimal.rustc
-            minimal.cargo
-            targets.${target}.latest.rust-std
-          ];
-          naerskCross = naersk.lib.${system}.override {
-            cargo = toolchain;
-            rustc = toolchain;
-          };
-        in
-        naerskCross.buildPackage {
-          src = ./.;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        target = "aarch64-unknown-linux-gnu";
+        toolchain = with fenix.packages.${system}; combine [
+          minimal.rustc
+          minimal.cargo
+          targets.${target}.latest.rust-std
+        ];
+        naerskCross = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+        cargoConfig = {
           CARGO_BUILD_TARGET = target;
           CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/${target}-gcc";
         };
-    });
+      in
+      {
+        defaultPackage = naerskCross.buildPackage ({
+          src = ./.;
+        } // cargoConfig);
+
+        devShell = pkgs.mkShell ({
+          name = "hyperpixel-init";
+
+          buildInputs = with pkgs; [
+            cargo-edit
+            nixpkgs-fmt
+            toolchain
+          ] ++ (with fenix.packages.${system}; [ rust-analyzer ]);
+        } // cargoConfig);
+      });
 }
