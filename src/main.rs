0a5e6bb53299a54cba0f7eabe11c11ddda30849b
Bernardo Meurer <bernardo@meurer.org>
2022-02-10 15:15:21 -0800

    chore(deps): flexi_logger 0.17.1 -> 0.22.3

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ use gpio::{Gpio, PinMode};
 use log::info;
 
 fn main() -> Result<()> {
-    Logger::with_env_or_str("info")
+    Logger::try_with_env_or_str("info")
+        .with_context(|| "Failed to parse log level")?
         .format(flexi_logger::colored_detailed_format)
         .set_palette("196;228;120;45;176".to_string())
         .start()
-        .with_context(|| "failed to initialize logger")?;
+        .with_context(|| "Failed to initialize logger")?;
 
     info!("HyperPixel 4 Initialization");
 
